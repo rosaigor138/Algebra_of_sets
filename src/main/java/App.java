9a269392390fbc4edeebb88b231b8873dff2f3b5
Igor Rosa <igorrosafilgueiras@gmail.com>
2023-04-15 23:25:42 -0300

    WIP

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,40 +1,171 @@
 import java.io.File;
 import java.util.Scanner;
+import domain.*;
+
 
 public class App {
     public static void main(String[] args) {
         Scanner option = new Scanner(System.in);
         int intChoice;
-        boolean boolChoice;
+        boolean boolChoice = true;
         String strChoice;
-
+        App app = new App();
+        Set sets;
         System.out.println("""
-                That program is created by Igor Rosa F. Pinto
-                Here you will see an algorithm responsible for making calculations involving Sets.
-                You like to create(1) manually the sets or import(2) from an .txt
-                Before you choose, if you choose the import option the txt that will be read is the 
-                'externalData.txt' in the package of the application.
-                
-                For that we have some rules to make that process more easy to you, write the data that way:
-                1, 2, 5 , 6 ,-220
-                -13, 14 , 22 
-                Insert on one line the data you wanna in every one of those 
-                
-                """);
+                    That program is created by Igor Rosa F. Pinto
+                    Here you will see an algorithm responsible for making calculations involving Sets.
+                    ***
+                    You like to create(1) manually the sets or import(2) from an .txt
+                    ***
+                    Before you choose, if you choose the import option the txt that will be read is the 
+                    'externalData.txt' in the package of the application.
+                    
+                    For that we have some rules to make that process more easy to you, write the data that way:
+                    1, 2, 5 , 6 ,-220
+                    -13, 14 , 22 
+                    
+                    1- Create manually the set
+                    2- Use the externalData.txt
+                    
+                    """);
         intChoice = option.nextInt();
-        if (intChoice == 1){
+            if (intChoice == 1) {
+                int set[][] = new int[3][];
+                set = app.create_manually_sets();
+                sets = new Set();
+
+                sets.setSetA(set[0]);
+                sets.setSetB(set[1]);
+                sets.setSetC(set[2]);
+                System.out.println("You have created your set");
+                while (boolChoice) {
+                    System.out.println("""
+                            1- To go to 'operations with set'
+                            2- To exit to exit the system
+                            """);
+                    intChoice = option.nextInt();
+                    if (intChoice == 1) {
+                        System.out.println("""
+                                1- Belongs
+                                2- 
+                                """);
+                        intChoice = option.nextInt();
+                        switch (intChoice){
+                            case 1:
+                                System.out.println("""
+                                        Enter two variables:
+                                        1- Enter with 0,1 or 2 for the set
+                                        2- Item to be compared
+                                        """);
+                                int intEnter = option.nextInt();
+                                System.out.println("2- Enter the value of the item to be compared");
+                                int intEnter2 = option.nextInt();
+                                if (sets.belongs(intEnter,intEnter2)){
+                                    System.out.println("Item "+intEnter2+" belongs to the set "+intEnter);
+                                }else{
+                                    System.out.println("Item "+intEnter2+"dont belongs to the set "+intEnter);
+                                }
+                            case 2:
+                                System.out.println("""
+                                        Enter two variables:
+                                        1- Enter with 0,1 or 2 for the first set
+                                        2- Enter with 0,1 or 2 for the second set
+                                        """);
+                                intEnter = option.nextInt();
+                                System.out.println("Enter the second set");
+                                intEnter2 = option.nextInt();
+                                int count = sets.contained(intEnter,intEnter2);
+                                if (count == sets.getSize(intEnter)){
+                                    if (sets.getSize(intEnter) == sets.getSize(intEnter2)){
+                                        System.out.println("Set 1 is contained in set 2");
+                                    }else {
+                                        System.out.println("Set 1 is properly contained in set 2");
+                                    }
+                                }else {
+                                    System.out.println("Set 1 is not contained in set 2");
+                                }
+                            case 3:
+
+                        }
+
+                    } else if (intChoice == 2) {
+                        throw new IllegalArgumentException("Thank you for testing this project");
+                    }
+                }
+            }
+
+            if (intChoice == 2){
+                System.out.println("""
+                        Reading data from 'externalData.txt'
+                        """);
+                File file = new File(
+                        "C:\\Users\\Igor\\Desktop\\github\\Algebra_of_sets\\src\\main\\java\\externalData.txt"
+                );
+            }
+            //else throw new IllegalArgumentException("Invalid option");
 
         }
-        if (intChoice == 2){
+
+    public void operations_with_set(){
+        Scanner scan = new Scanner(System.in);
+        boolean bool = true;
+        while(bool){
             System.out.println("""
-                    Reading data from 'externalData.txt'
+                    There are the options of the system
                     """);
-            File file = new File(
-                    "C:\\Users\\Igor\\Desktop\\github\\Algebra_of_sets\\src\\main\\java\\externalData.txt"
-            );
         }
-        if (intChoice != 1 || intChoice != 2){
-            throw new IllegalArgumentException("You typed an wrong option");
+    }
+
+    public int[][] create_manually_sets() {
+        Scanner option = new Scanner(System.in);
+        int intScan;
+        int itemFromUser;
+
+
+        System.out.println("You choose the option to create manually the Set");
+        System.out.println("""
+                We can create 3 sets:
+                1- To create 1 set
+                2- To create 2 sets
+                3- To create 3 sets
+                """);
+        intScan = option.nextInt();
+
+        int[][] sets = new int[3][];
+
+        switch (intScan) {
+            case 1:
+                sets[0] = create_set();
+                System.out.println("You created your set");
+                break;
+            case 2:
+                sets[0] = create_set();
+                sets[1] = create_set();
+                System.out.println("You created your set");
+                break;
+            case 3:
+                sets[0] = create_set();
+                sets[1] = create_set();
+                sets[2] = create_set();
+                System.out.println("You created your set");
+                break;
         }
+        return sets;
+    }
+
+
+
+    public int[] create_set(){
+        Scanner option = new Scanner(System.in);
+        int intChoice;
+        System.out.println("Enter the size of your set");
+        intChoice= option.nextInt();
+        int[] set = new int[intChoice];
+        for (int i = 0; i < intChoice; i++){
+            System.out.println("Enter the value of the [" + i + "] position");
+            set[i] = option.nextInt();
+        }
+        System.out.println();
+        return set;
     }
 }
--- src/main/java/domain/Set.java
+++ src/main/java/domain/Set.java
@@ -1,22 +1,109 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class Set {
-    private ArrayList<Integer> data= new ArrayList<>();
-    private String label;
-    private Map<String,List> set;
-    public Set(List<Integer> data, String label){
+    private int setA[];
+    private int setB[];
+    private int setC[];
+    private int invalidValueRepresantation = 999999;
+    public Set(){
+        this.setA = new int[0];
+        this.setB = new int[0];
+        this.setC = new int[0];
+    }
 
-        for (int i = 0;i < data.size(); i++){
-        this.data.add(data.indexOf(i));
+    public void setSetA(int[] setA) {
+        this.setA = setA;
+    }
+
+    public void setSetB(int[] setB) {
+        this.setB = setB;
+    }
+
+    public void setSetC(int[] setC) {
+        this.setC = setC;
+    }
+    public int getSize(int set){
+        if (set == 0){return setA.length;}if (set == 1){return setB.length;}if (set == 2){return setC.length;}
+        return 0;
+    }
+
+    /*
+    public void normalizeSet(){
+        int comparedItem;
+        for (int i = 0; i < setA.length; i++){
+            comparedItem = setA[i];
+            for (int j = 0; j < setA.length-1; j++){
+                if (setA[i] != j){
+                    if (comparedItem == setA[j]){
+                        setA[j]
+                    }
+                }
+            }
         }
-
     }
 
-    public ArrayList<Integer> getData() {
-        return data;
+     */
+
+    public int[] union(int set1, int set2){
+        int auxiliar1[] = new int[0];
+        int auxiliar2[] = new int[0];
+        int auxiliar3[] = new int[0];
+        if (set1 == 0){auxiliar1 = setA;}if (set1 == 1){auxiliar1 = setB;}if (set1 == 2){auxiliar1 = setC;}
+        if (set2 == 0){auxiliar2 = setA;}if (set1 == 1){auxiliar2 = setB;}if (set1 == 2){auxiliar2 = setC;}
+        ///ArrayList<Integer> result = new ArrayList<Integer>(Arrays.asList(auxiliar1));
+        for (int i =0; i < auxiliar2.length; i ++){
+            if (!belongs(set1,auxiliar2[i])){
+
+            }
+        }return auxiliar1;// temp
     }
-}
+    public int contained(int set1, int set2){
+        int count = 0;
+        int auxiliar1[] = new int[0];
+        if (set1 == 0){auxiliar1 = setA;}if (set1 == 1){auxiliar1 = setB;}if (set1 == 2){auxiliar1 = setC;}
+        for (int i = 0; i < auxiliar1.length; i++){
+            if (belongs(set2, auxiliar1[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+    public boolean belongs(int set, int x){
+        if (set == 0){
+            for (int i = 0; i < setA.length; i++){
+                if (x == setA[i]){return true;}
+            }
+        }
+        if (set == 1){
+            for (int i = 0; i < setB.length; i++){
+                if (x == setB[i]){return true;}
+            }
+        }
+        if (set == 2){
+            for (int i = 0; i < setC.length; i++){
+                if (x == setC[i]){return true;}
+            }
+        }
+        return false;
+    }
+    public void printAllData() {
+        int[][] data = new int[3][];
+        data[0] = this.setA;
+        data[1] = this.setB;
+        data[2] = this.setC;
+        try{
+            for (int[] arr: data) {
+                for (int n : arr) {
+                    System.out.print(n + " ");
+                }
+            }}
+        catch(Exception e){
+            System.out.println("");
+            }
+        }
+    }
+
